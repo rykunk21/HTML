e42eb67fd0bf519abdf38e8bec859857b09149f4
rykunk21 <rykunk21@gmail.com>
2021-03-18 13:52:36 -0400

    main

--- CSE-232/badProg.cpp
+++ CSE-232/badProg.cpp
@@ -1,29 +1,18 @@
-#include <vector>
-using std::vector;
-#include <string>
-using std::string; using std::to_string;
 #include <iostream>
-using std::cout; using std::endl;
+#include <vector>
 
-int find_and_return_max(vector<int> vector1, int max) {
-    if (vector1.size() == 0)
-        return -1;
-    max = vector1[0];
-    for (auto i = vector1.begin(); i != vector1.end(); ++i) {
-        if (max < *i) {
-            max = *i;
-        }
-    }
-    return 0;
+// Define first_three_multiples() here:
+std::vector<int> first_three_multiples(int num){
+  std::vector<int> out(3);
+  for(int i=1; i <= 3; i++){
+    out.push_back(num * i);
+  }
 }
 
 int main() {
-    vector<int> myvec = {17, 21, 44, 2, 60};
-    int max = myvec[0];
-    if (find_and_return_max(myvec, max) != 0) {
-        cout << "strange error" << endl;
-        return -1;
-    }
-    cout << "max value in the vector is " << to_string(max) << endl;
-    return 0;
+  
+  for (int element : first_three_multiples(8)) {
+    std::cout << element << "\n";
+  }
+  
 }
